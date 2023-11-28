71d28bc6a56aa7cbd0fb6e5a0d04de8f6d729f58
Felix <findech6@gmail.com>
2023-11-28 22:28:55 +0300

    completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,43 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 'apple', True, 3.14]
 
-def add_element_to_end_of_list(l, element):
-    return None
+    
+def add_element_to_end_of_list(input_list, element):
+    input_list.append(element)
+    return input_list
 
-def add_element_to_start_of_list(l, element):
-    return None
 
-def remove_element_from_end_of_list(l):
-    return None
+def add_element_to_start_of_list(input_list, element):
+    input_list.insert(0, element)
+    return input_list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_end_of_list(input_list):
+    if input_list:  # Check if the list is not empty
+        input_list.pop()
+    return input_list
 
-def retrieve_first_element_from_list(l):
-    return None
+def remove_element_from_start_of_list(input_list):
+    if input_list:  # Check if the list is not empty
+        del input_list[0]
+    return input_list
+
+def retrieve_first_element_from_list(input_list):
+    if input_list:  # Check if the list is not empty
+        return input_list[0]
+    else:
+        return None  # Return None if the list is empty
 
 def retrieve_element_from_index(l, index):
-    return None
+    if l and 0 <= index < len(l):
+        return l[index]
+    else:
+        return None
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(input_list):
+    if input_list:  # Check if the list is not empty
+        return input_list[-1]  # Use negative indexing to get the last element
+    else:
+        return None  # Return None if the list is empty
